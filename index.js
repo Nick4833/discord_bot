fd275bfe3df474d194210f83f84b8b943d7f2180
Nick <andrews4833@gmal.com>
2023-01-11 22:12:55 +0630

    host fixed

--- index.js
+++ index.js
@@ -94,6 +94,6 @@ app.get("/", function (req, res) {
   res.send("Hello World");
 });
 
-app.listen(3000, () => {
+app.listen(3000,'0.0.0.0', () => {
   console.log(`Example app listening on port 3000`);
 });
